2578600cebfa47683a64960171e663769f7006d4
pavel asadchy <asadchy.pavel@gmail.com>
2021-04-11 22:15:55 +0300

    style: footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,28 +1,18 @@
-import { ListGroup } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Footer.scss';
 
 const Footer: React.FC = () => {
   return (
-    <div className="bg-light container footer ">
-      <div className="row">
-        <ListGroup horizontal>
-          <ListGroup.Item>
-            <a href="https://github.com/PavelAsadchy">Pavel Asadchy</a>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <a href="https://github.com/KaguraDun">Vasily Kovnev</a>
-          </ListGroup.Item>
-        </ListGroup>
-      </div>
-
-      <span className="row">2021</span>
-
-      <div className="row">
-        <img alt="" className=" logo" src="../../rs_school_js.svg" />
-      </div>
-    </div>
+    <Navbar bg="dark" className="footer" variant="dark">
+      <Nav>
+        <Nav.Link href="https://github.com/PavelAsadchy">
+          Pavel Asadchy
+        </Nav.Link>
+      </Nav>
+    </Navbar>
   );
 };
 
